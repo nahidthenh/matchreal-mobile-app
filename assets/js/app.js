6d199241adc8caca33cb021e0c7ff9c640c0559b
nahidthenh <nhgroup18@gmail.com>
2023-05-17 12:43:32 +0600

    File Setup

---
+++ assets/js/app.js
@@ -0,0 +1,16 @@
+// To show the preloader
+function showPreloader() {
+    document.getElementById('preloader').style.display = 'flex';
+}
+
+// To hide the preloader
+function hidePreloader() {
+    document.getElementById('preloader').style.display = 'none';
+}
+
+// Example usage:
+// Show preloader when some time-consuming task starts
+showPreloader();
+
+// Hide preloader when the task completes
+setTimeout(hidePreloader, 1000); // Assuming the task takes 3 seconds
